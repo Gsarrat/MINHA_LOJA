e929dc24e2fe767b1b4308bdf680cb92666c6831
gabriel sarrat <gabriel.sarrat@gmail.com>
2023-09-10 11:32:13 -0300

    33.0 & 34.0

--- loja/clientes/rotas.py
+++ loja/clientes/rotas.py
@@ -1,10 +1,11 @@
-from flask import redirect, render_template, url_for, flash, request, session, current_app
+from flask import redirect, render_template, url_for, flash, request, session, current_app, make_response
 from flask_bcrypt import Bcrypt
 from loja import db, app, photos, bcrypt, login_manager
 from .forms import CadastroClienteForm, ClienteLoginForm
 import secrets, os
 from .models import Cadastrar, ClientePedido
 from flask_login import login_required, current_user, login_user, logout_user
+import pdfkit  
 
 
 @app.route('/cliente/cadastrar', methods=['GET', 'POST'])
@@ -67,7 +68,7 @@ def pedido_order():
             session.pop('LojainCarrinho')
             flash('Seu pedido foi salvo com sucesso', 'success')
 
-            return redirect(url_for('home'))
+            return redirect(url_for('pedidos', notafiscal=notafiscal))
         except Exception as e:
             print(e)
             flash('Pedido nao processado', 'danger')
@@ -93,5 +94,32 @@ def pedidos(notafiscal):
         return redirect(url_for('clenteLogin'))
     return render_template('cliente/pedido.html', notafiscal=notafiscal, imposto=imposto,subTotal=subTotal, gTotal=gTotal,cliente=cliente, pedidos=pedidos)
 
+# Issso é a implementacao da NOTA fiscal em PDF
+@app.route('/get_pdf/<notafiscal>', methods=['POST'])
+@login_required
+def get_pdf(notafiscal):
+    if current_user.is_authenticated:
+        gTotal = 0
+        subTotal = 0
+        cliente_id = current_user.id
+        if request.method =="POST":
+            cliente = Cadastrar.query.filter_by(id=cliente_id).first()
+            pedidos = ClientePedido.query.filter_by(cliente_id=cliente_id, notafiscal=notafiscal).order_by(ClientePedido.id.desc()).first()
+            for _key, produto in pedidos.pedido.items():
+                disconto = (produto['discount']/100) * float(produto['price'])
+                subTotal += float(produto['price']* int(produto['quantity']))
+                subTotal -= disconto
+                imposto = ("%.2f" % (.00* float(subTotal))) #<--------------altera aliquota de imposto
+                gTotal = float("%.2f" % (1.06 * subTotal)) #<--------------- conferir o calculo
 
-        
+
+            redered =  render_template('cliente/pdf.html', notafiscal=notafiscal, imposto=imposto,subTotal=subTotal, gTotal=gTotal,cliente=cliente, pedidos=pedidos)
+            config = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
+            pdf = pdfkit.from_string(redered, False, configuration=config)
+            response = make_response(pdf)
+            response.headers['Content-Type'] = 'application/pdf'
+            response.headers['Content-Disposition'] = f'attched; filename={notafiscal}.pdf'
+
+            return response
+    return redirect(url_for('pedidos'))
+
--- run.py
+++ run.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
 
 
 
-# 32.0
-# Add - Botao de realizar o pedido carros.html
-# Add - pedidos sendo apensados no banco cliente_pedido
-# Add - pagina pedido/*numero do pedido* exibe a confirmacao da compra
-# Err - Subtotal nao aprece em, pedido/*numero do pedido*
+# 33.0 e 34.0
+
+# Add - Download do PDF
